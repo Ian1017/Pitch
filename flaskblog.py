58246366cc72799bb718fda5118894124aabefed
ian1017 <mdawidamengo@gmail.com>
2020-01-14 09:14:37 +0300

    setting up the templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
